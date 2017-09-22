482dec61de29eccd34fb2bae6a8899eaae425d67
Rob Rankin <rob.rankin.jr@gmail.com>
2017-09-22 11:14:51 -0400

    m1capstone

--- m1-w4d4-c-capstone/CapstoneTests/UnitTest1.cs
+++ m1-w4d4-c-capstone/CapstoneTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone;
 
 namespace CapstoneTests
 {
@@ -7,8 +8,17 @@ namespace CapstoneTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void VendingMachineTests()
         {
+            VendingMachine myVending = new VendingMachine();
+            CommandLineInterface myCLI = new CommandLineInterface(myVending);
+            myVending.FeedMoney(10);
+            Assert.AreEqual(10.00.ToString(), myVending.Balance.ToString());
+
+            Item myItem = myVending.FindItem("A3");
+            myCLI.Purchase(myItem);
+            Assert.AreEqual(7.25.ToString(), myVending.Balance.ToString());
+
         }
     }
 }
